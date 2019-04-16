313bd87245e78816b021a7635194d27c886b11e0
Jayson Hairston <jhairston@pivotal.io>
2019-04-16 12:17:54 -0400

    Separate out terraform show into yaml and bash files

--- terraform_show_statefile.sh
+++ terraform_show_statefile.sh
@@ -3,7 +3,7 @@
 cd terraform-template-aws/pivotal-cf-terraforming-aws*/terraforming-pas
 pwd
 ls -lrtha
-cp terraform-state-file/((terraform_statefile)) terraform.tfstate
+cp terraform-state-file/$terraform_statefile terraform.tfstate
 pwd
 ls -lrtha
 echo "Catting Local Satefile:"
