1267f2b759f04fa39c82676d2a7156f80dc13f0c
Jayson Hairston <jhairston@pivotal.io>
2019-04-16 14:02:55 -0400

    Modify cp of the statefile position

--- terraform_show_statefile.sh
+++ terraform_show_statefile.sh
@@ -4,10 +4,10 @@ ls -lrtha
 ls -lrtha terraform-state-file
 pwd
 echo Terraform Statefile name: $terraform_statefile
-cp terraform-state-file/$terraform_statefile terraform-template-aws/pivotal-cf-terraforming-aws*/terraforming-pas/terraform.tfstate
 
 cd terraform-template-aws/pivotal-cf-terraforming-aws*/terraforming-pas
 pwd
+cp ../../../terraform-state-file/$terraform_statefile terraform.tfstate
 ls -lrtha
 
 echo "Catting Local Satefile:"
