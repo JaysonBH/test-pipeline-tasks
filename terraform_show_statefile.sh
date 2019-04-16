b872176adc13175fb4cff0ebf58f13046eaa1a81
Jayson Hairston <jhairston@pivotal.io>
2019-04-16 10:47:33 -0400

    Add Terraform Show Task

---
+++ terraform_show_statefile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd terraform-template-aws/pivotal-cf-terraforming-aws*/terraforming-pas
+pwd
+ls -lrtha
+cp terraform-state-file/((terraform_statefile)) terraform.tfstate
+pwd
+ls -lrtha
+echo "Catting Local Satefile:"
+cat terraform.tfstate
+echo "Executing Terraform Show:"
+terraform show
+
+exit 0
