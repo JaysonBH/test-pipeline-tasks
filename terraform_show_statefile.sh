ff300494dc567ddab388782dc0c5a4f3c8429fed
Jayson Hairston <jhairston@pivotal.io>
2019-04-16 12:55:21 -0400

    re-arrange terraform show

--- terraform_show_statefile.sh
+++ terraform_show_statefile.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+cp terraform-state-file/$terraform_statefile terraform-template-aws/pivotal-cf-terraforming-aws*/terraforming-pas/terraform.tfstate
+
 cd terraform-template-aws/pivotal-cf-terraforming-aws*/terraforming-pas
 pwd
 ls -lrtha
-cp terraform-state-file/$terraform_statefile terraform.tfstate
-pwd
-ls -lrtha
+
 echo "Catting Local Satefile:"
 cat terraform.tfstate
+
 echo "Executing Terraform Show:"
 terraform show
 
