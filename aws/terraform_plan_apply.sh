60b98a9163cce6a10a39ce0f7c857eff451f4388
Jayson Hairston <jhairston@pivotal.io>
2019-04-30 11:16:14 -0400

    change az

--- aws/terraform_plan_apply.sh
+++ aws/terraform_plan_apply.sh
@@ -11,7 +11,8 @@ echo "availability_zones=$availability_zones" > variable.txt
 echo "cating variable.txt"
 cat variable.txt
 
-terraform plan -var "env_name"=$env_name -var "access_key"=$access_key -var "secret_key"=$secret_key -var "region"=$region -var "ops_manager_ami"=$ops_manager_ami -var "dns_suffix"=$dns_suffix -out terraform.tfplan -var-file=variable.txt -state=../../../terraform-state-file/$terraform_statefile
+#terraform plan -var "env_name"=$env_name -var "access_key"=$access_key -var "secret_key"=$secret_key -var "region"=$region -var "ops_manager_ami"=$ops_manager_ami -var "dns_suffix"=$dns_suffix -out terraform.tfplan -var-file=variable.txt -state=../../../terraform-state-file/$terraform_statefile
+terraform plan -var "env_name"=$env_name -var "access_key"=$access_key -var "secret_key"=$secret_key -var "region"=$region -var "ops_manager_ami"=$ops_manager_ami -var "dns_suffix"=$dns_suffix -out terraform.tfplan -var="availability_zones"=$availability_zones -state=../../../terraform-state-file/$terraform_statefile
 
 terraform apply terraform.tfplan
 
