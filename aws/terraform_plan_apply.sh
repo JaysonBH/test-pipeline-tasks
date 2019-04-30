8b8dacb5e80b593f865cf759cc886c867cce7082
Jayson Hairston <jhairston@pivotal.io>
2019-04-30 13:24:50 -0400

    Modify terraform plan to include cert variables

--- aws/terraform_plan_apply.sh
+++ aws/terraform_plan_apply.sh
@@ -7,11 +7,13 @@ echo "Original state file Existance:"
 ls -l ../../../terraform-state-file/$terraform_statefile
 
 echo "availability_zones=$availability_zones" > variable.txt
+echo "ssl_cert=$ssl_cert" >> variable.txt
+echo "ssl_private_key=$ssl_private_key" >> variable.txt
 ls -lrtha
 echo "cating variable.txt containing Availability Zones"
 cat variable.txt
 
-terraform plan -var "env_name"=$env_name -var "access_key"=$access_key -var "secret_key"=$secret_key -var "region"=$region -var "ops_manager_ami"=$ops_manager_ami -var "dns_suffix"=$dns_suffix -var "ssl_cert"=$ssl_cert -var "ssl_private_key"=$ssl_private_key -out terraform.tfplan -var-file=variable.txt -state=../../../terraform-state-file/$terraform_statefile
+terraform plan -var "env_name"=$env_name -var "access_key"=$access_key -var "secret_key"=$secret_key -var "region"=$region -var "ops_manager_ami"=$ops_manager_ami -var "dns_suffix"=$dns_suffix -out terraform.tfplan -var-file=variable.txt -state=../../../terraform-state-file/$terraform_statefile
 
 terraform apply terraform.tfplan
 
