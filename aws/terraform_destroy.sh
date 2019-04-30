c52cfd01cd846e944149f2a69cbe33592e555292
Jayson Hairston <jhairston@pivotal.io>
2019-04-30 13:05:41 -0400

    Add Terraform Destroy for aws

---
+++ aws/terraform_destroy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd terraform-template-aws/pivotal-cf-terraforming-aws*/terraforming-pas
+terraform init
+
+echo "Original state file Existance:"
+ls -l ../../../terraform-state-file/$terraform_statefile
+
+echo "Copy Existing Statefile to local directory:"
+cp ../../../terraform-state-file/$terraform_statefile terraform.tfstate
+
+echo "availability_zones=$availability_zones" > variable.txt
+ls -lrtha
+echo "cating variable.txt containing Availability Zones"
+cat variable.txt
+
+echo "catting local statefile Before Destroy"
+cat terraform.tfstate
+
+terraform destroy -auto-approve -var "env_name"=$env_name -var "access_key"=$access_key -var "secret_key"=$secret_key -var "region"=$region -var "ops_manager_ami"=$ops_manager_ami -var "dns_suffix"=$dns_suffix -var "ssl_cert"=$ssl_cert -var "ssl_private_key"=$ssl_private_key -var-file=variable.txt
+
+echo "Copying local statefile to updated location"
+cp terraform.tfstate ../../../updated-state-file/$terraform_statefile
+echo "Catting updated state file"
+cat ../../../updated-state-file/$terraform_statefile
+echo "Showing updated state file"
+ls -lrth ../../../updated-state-file/
+
+
+exit 0
