e6a3608ee8d6b33fef824c03e84af5a6cf20aa58
Jayson Hairston <jhairston@pivotal.io>
2019-04-30 11:04:38 -0400

    substitute each Concourse var with a bash variable

--- aws/terraform_plan_apply.sh
+++ aws/terraform_plan_apply.sh
@@ -7,11 +7,11 @@ ls -l ../../../terraform-state-file/$terraform_statefile
 echo "Catting Original state file:"
 cat ../../../terraform-state-file/$terraform_statefile
 ls -lrtha
-echo "availability_zones=((availability_zones))" > variable.txt
+echo "availability_zones=$availability_zones" > variable.txt
 echo "cating variable.txt"
 cat variable.txt
 
-terraform plan -var "env_name"=((env_name)) -var "access_key"=((access_key)) -var "secret_key"=((secret_key)) -var "region"=((region)) -var "ops_manager_ami"=((ops_manager_ami)) -var "dns_suffix"=((dns_suffix)) -out terraform.tfplan -var-file=variable.txt -state=../../../terraform-state-file/$terraform_statefile
+terraform plan -var "env_name"=$env_name -var "access_key"=$access_key -var "secret_key"=$secret_key -var "region"=$region -var "ops_manager_ami"=$ops_manager_ami -var "dns_suffix"=$dns_suffix -out terraform.tfplan -var-file=variable.txt -state=../../../terraform-state-file/$terraform_statefile
 
 terraform apply terraform.tfplan
 
