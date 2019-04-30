8997be32bbb40a5e979e2f68430a95e9080d4965
Jayson Hairston <jhairston@pivotal.io>
2019-04-30 12:16:36 -0400

    Add SSL Cert and Key Variables

--- aws/terraform_plan_apply.sh
+++ aws/terraform_plan_apply.sh
@@ -11,7 +11,7 @@ echo "availability_zones=$availability_zones" > variable.txt
 echo "cating variable.txt"
 cat variable.txt
 
-terraform plan -var "env_name"=$env_name -var "access_key"=$access_key -var "secret_key"=$secret_key -var "region"=$region -var "ops_manager_ami"=$ops_manager_ami -var "dns_suffix"=$dns_suffix -out terraform.tfplan -var-file=variable.txt -state=../../../terraform-state-file/$terraform_statefile
+terraform plan -var "env_name"=$env_name -var "access_key"=$access_key -var "secret_key"=$secret_key -var "region"=$region -var "ops_manager_ami"=$ops_manager_ami -var "dns_suffix"=$dns_suffix -var "ssl_cert"=$ssl_cert -var "ssl_private_key"=$ssl_private_key -out terraform.tfplan -var-file=variable.txt -state=../../../terraform-state-file/$terraform_statefile
 
 terraform apply terraform.tfplan
 
