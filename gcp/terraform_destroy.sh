78fd1a1d6c235dc69b843b108f1781d5ff3f1bcd
Jayson Hairston <jhairston@pivotal.io>
2019-05-13 14:06:20 -0400

    Add service_account_key to gcp destroy II

--- gcp/terraform_destroy.sh
+++ gcp/terraform_destroy.sh
@@ -20,7 +20,7 @@ cat terraform.tfstate
 terraform destroy -auto-approve -var "env_name"=$env_name \
   -var "project"=$project -var "region"=$region -var-file=variable.txt \
   -var "dns_suffix"=$dns_suffix -var "opsman_image_url"=$opsman_image_url \
-  -var "service_account_key"=$service_account_key
+  -var "service_account_key"=$service_account_key \
   -state=terraform.tfstate
 
 echo "Copying local statefile to updated location"
