1bd2685a65d7bee67dcd9b1eb00d58252b34b3b7
Jayson Hairston <jhairston@pivotal.io>
2019-05-13 16:59:43 -0400

    Add echo comment

--- gcp/terraform_plan_apply.sh
+++ gcp/terraform_plan_apply.sh
@@ -18,6 +18,7 @@ cat variable.txt
 #terraform plan -var "env_name"=$env_name -var "access_key"=$access_key -var "secret_key"=$secret_key -var "region"=$region -var "ops_manager_ami"=$ops_manager_ami -var "dns_suffix"=$dns_suffix -out terraform.tfplan -var-file=variable.txt -state=../../../terraform-state-file/$terraform_statefile
 terraform plan -var "env_name"=$env_name -var "project"=$project -var "region"=$region -var "dns_suffix"=$dns_suffix -var "opsman_image_url"=$opsman_image_url -var-file=variable.txt -state=terraform.tfstate -out=terraform.tfplan
 
+echo "Applying Terraform Plan"
 terraform apply terraform.tfplan
 
 echo "catting local statefile, if exists"
