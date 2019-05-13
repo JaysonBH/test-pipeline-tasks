8ac4761f22bfa7c77755d4970c71f5cb73f82fb9
Jayson Hairston <jhairston@pivotal.io>
2019-05-13 15:36:50 -0400

    Update vars to terraform plan gcp

--- gcp/terraform_plan_apply.sh
+++ gcp/terraform_plan_apply.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-cd terraform-template-aws/pivotal-cf-terraforming-aws*/terraforming-pas
+cd terraform-template-gcp/pivotal-cf-terraforming-*/terraforming-pas
 terraform init
 
 echo "Original state file Existance:"
 ls -l ../../../terraform-state-file/$terraform_statefile
 
+echo "Copy Existing Statefile to local directory:"
+cp ../../../terraform-state-file/$terraform_statefile terraform.tfstate
+
 echo "availability_zones=$availability_zones" > variable.txt
-echo "ssl_cert = $ssl_cert" >> variable.txt
-echo "ssl_private_key =$ssl_private_key" >> variable.txt
+echo "service_account_key=$service_account_key" > variable.txt
 ls -lrtha
-echo "cating variable.txt containing Availability Zones"
+echo "cating variable.txt containing Availability Zones and service account key"
 cat variable.txt
 
 #terraform plan -var "env_name"=$env_name -var "access_key"=$access_key -var "secret_key"=$secret_key -var "region"=$region -var "ops_manager_ami"=$ops_manager_ami -var "dns_suffix"=$dns_suffix -out terraform.tfplan -var-file=variable.txt -state=../../../terraform-state-file/$terraform_statefile
-terraform plan -var "env_name"=$env_name -var "access_key"=$access_key -var "secret_key"=$secret_key -var "region"=$region -var "ops_manager_ami"=$ops_manager_ami -var "dns_suffix"=$dns_suffix -out terraform.tfplan -var="availability_zones"=$availability_zones -state=../../../terraform-state-file/$terraform_statefile
+terraform plan \
+  -var "env_name"=$env_name -var "project"=$project -var "region"=$region \
+  -var "dns_suffix"=$dns_suffix -var "opsman_image_url"=$opsman_image_url \
+  -var-file=variable.txt -out=terraform.tfplan -state=terraform.tfstate
 
 terraform apply terraform.tfplan
 
